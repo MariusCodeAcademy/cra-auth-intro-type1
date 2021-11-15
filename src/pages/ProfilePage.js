7d29b4e5240609d2d7c9ec85fdabef5f1b87fd91
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-15 12:00:47 +0200

    b4 break

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,4 +1,10 @@
 function ProfilePage() {
+  // to get profile info we send toke with fetch
+
+  // get all users posts from
+  // get http://localhost:5000/posts
+  // send token
+
   return (
     <main>
       <h1>profile</h1>
--- src/utils/Fetch.js
+++ src/utils/Fetch.js
@@ -5,6 +5,7 @@ export async function login(email, password) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      // Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAYi5jb20iLCJpYXQiOjE2MzY5Njk5NjEsImV4cCI6MTYzNjk3MzU2MX0.Gv2_RAdnOymFlDqgJOwNReTjsrqKIZZBTnknij5_8Bs'
     },
     body: JSON.stringify({
       email,
